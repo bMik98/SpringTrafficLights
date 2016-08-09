aa5467c21063f43317a4d5757547802394e44d85
bMik98 <bmik9898@gmail.com>
2016-08-10 01:00:49 +0300

    console version - the second pass

--- src/main/java/io/khasang/incubator/SpringTrafficLights/model/Light/GreenBlinkingLight.java
+++ src/main/java/io/khasang/incubator/SpringTrafficLights/model/Light/GreenBlinkingLight.java
@@ -1,4 +1,10 @@
 package io.khasang.incubator.SpringTrafficLights.model.Light;
 
-public class GreenBlinkingLight {
+import org.springframework.stereotype.Component;
+
+@Component("greenBlinkingLight")
+public class GreenBlinkingLight extends Light {
+    public GreenBlinkingLight() {
+        super(Colour.GREEN, Mode.BLINKING);
+    }
 }
